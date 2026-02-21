09074227b9a35194de009ee7aa5963943f4684bc
vicriment <ocetaq@gmail.com>
2026-02-21 19:24:54 +0530

    update consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,10 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_URL = "https://vierresc.github.io";
+export const SITE_URL = "https://vicriment.github.io";
 export const SITE_TITLE = "vicriment's void";
 export const SITE_DESCRIPTION =
   "thoughts & prayers";
 
 export const EMAIL = "sthirkal@gmail.com";
+
